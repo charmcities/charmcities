0da6cf717f5b215871978d3fefc4d5e7dbad9e46
Martin Sherman-Marks <mmarks@ghotinet.net>
2020-01-30 00:45:02 -0500

    Add button tooltips
    Added a very simple tooltip to assist players in determining what key buttons do.

---
+++ Assets/Scripts/ButtonTooltip.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+
+public class ButtonTooltip : MonoBehaviour
+{
+    TextMeshProUGUI tooltipTextbox;
+    RectTransform background;
+
+    BaseControls controls;
+    Vector2 mousePos;
+    [SerializeField]
+    private Camera uiCamera;
+
+    readonly float textPadding = 4f;
+    float cursorXOffset = 10f;
+    float cursorYOffset = -25f;
+
+    private void Awake()
+    {
+        background = transform.Find("Background").GetComponent<RectTransform>();
+        tooltipTextbox = transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>();
+        
+        Hide();
+
+        // Unity New Input System actions
+        controls = new BaseControls();
+        // Track mouse position
+        controls.Navigation.PointerLocation.performed += ctx => mousePos = ctx.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        // Keep tooltip fully contained within screen bounds.
+        if (mousePos.x + cursorXOffset < 0)
+        {
+            mousePos.x = -cursorXOffset;
+        }
+        else if (mousePos.x + cursorXOffset + background.sizeDelta.x > Screen.width)
+        {
+            mousePos.x = Screen.width - (cursorXOffset + background.sizeDelta.x);
+        }
+        if (mousePos.y + cursorYOffset - background.sizeDelta.y < 0)
+        {
+            mousePos.y = -cursorYOffset + background.sizeDelta.y;
+        }
+        else if (mousePos.y + cursorYOffset > Screen.height)
+        {
+            mousePos.y = Screen.height - cursorYOffset;
+        }
+
+        Vector2 localPos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(transform.parent.GetComponent<RectTransform>(), mousePos, uiCamera, out localPos);
+        localPos.x += cursorXOffset;
+        localPos.y += cursorYOffset;
+
+        transform.localPosition = localPos;
+    }
+
+    public void Show(string tooltipString)
+    {
+        gameObject.SetActive(true);
+
+        tooltipTextbox.SetText(tooltipString);
+
+        Vector2 backgroundSize = new Vector2(tooltipTextbox.preferredWidth + (textPadding * 2), tooltipTextbox.preferredHeight + (textPadding * 2));
+        background.sizeDelta = backgroundSize;
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        if (controls != null)
+        {
+            controls.Disable();
+        }
+    }
+}
---
+++ Assets/Scripts/ButtonTooltipMouseover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonTooltipMouseover : MonoBehaviour
+{
+    public ButtonTooltip tooltipUI;
+    [TextArea(3,10)]
+    public string text;
+
+    private EventTrigger trigger { get { return GetComponent<EventTrigger>(); } }
+
+    private void Awake()
+    {
+        gameObject.AddComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerEnter;
+        entry.callback.AddListener((data) => { OnPointerEnter((PointerEventData)data); });
+        trigger.triggers.Add(entry);
+
+        EventTrigger.Entry exit = new EventTrigger.Entry();
+        exit.eventID = EventTriggerType.PointerExit;
+        exit.callback.AddListener((data) => { OnPointerExit((PointerEventData)data); });
+        trigger.triggers.Add(exit);
+    }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+        tooltipUI.Show(text);
+    }
+
+    public void OnPointerExit(PointerEventData data)
+    {
+        tooltipUI.Hide();
+    }
+}
